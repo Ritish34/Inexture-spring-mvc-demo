4143b4ce66afa287a46bf8743ca76eaabef36d1f
Ritish <ritish.inexture@gmail.com>
2023-03-21 12:49:12 +0530

    intial commit

---
+++ src/main/java/com/inexture/flightserchingapi/FlightSerchingApiApplication.java
@@ -0,0 +1,13 @@
+package com.inexture.flightserchingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightSerchingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightSerchingApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inexture/flightserchingapi/FlightSerchingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inexture.flightserchingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightSerchingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
